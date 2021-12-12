0ae6b6a87efb77cd7169a609eafd27856e346624
Pauladls <delossantos.paula.g@gmail.com>
2021-12-12 01:14:27 -0300

    first commit

---
+++ JS/Index.js
@@ -0,0 +1,40 @@
+function antes(numero) {
+    let home1 = document.querySelector("#home");
+    let proyectos = document.querySelector("#proyectos");
+    let nos = document.querySelector("#nos");
+    let contacto = document.querySelector("#contacto");
+
+    if (numero == home1){
+        home1.innerHTML = "HOME";
+    }
+    else if (numero == proyectos){
+        proyectos.innerHTML = "PROYECTOS";
+    }
+    else if (numero == nos){
+        nos.innerHTML = "NOSOTRAS";
+    }
+    else{
+        (numero == contacto)
+          contacto.innerHTML = "CONTACTO";
+        }
+}
+function despues(numero) {
+    let home1 = document.querySelector("#home");
+    let proyectos = document.querySelector("#proyectos");
+    let nos = document.querySelector("#nos");
+    let contacto = document.querySelector("#contacto");
+
+    if (numero == home1){
+        home1.innerHTML = "001";
+    }
+    else if (numero == proyectos){
+        proyectos.innerHTML = "002";
+    }
+    else if (numero == nos){
+        nos.innerHTML = "003";
+    }
+    else{
+        (numero == contacto)
+          contacto.innerHTML = "004";
+        }
+}
