eec033a128a2f8a7faa88dac069138b42172cadd
Pauladls <delossantos.paula.g@gmail.com>
2021-12-13 18:52:56 -0300

    Logo izquierda

--- JS/Index.js
+++ JS/Index.js
@@ -1,3 +1,66 @@
+/*HEADER*/
+document.querySelector(".containerheader").innerHTML= `
+<h5>ARQUITECTURA & DISEÑO</h5>
+`
+/*ASIDE1*/
+document.querySelector(".containeraside1").innerHTML=`
+
+<div>                        
+<div>
+    <i class="fas fa-angle-double-right"></i>
+     <a class="cambio" id="home" onmouseover="antes(home)" onmouseout="despues(home)"  href="index.html">001</a>
+</div>
+</div>
+<div>                        
+<div>
+    <i class="fas fa-angle-double-right"></i>
+     <a class="cambio" id="proyectos" onmouseover="antes(proyectos)" onmouseout="despues(proyectos)"  href="proyectos.html">002</a>
+</div>
+</div>
+
+<div>                        
+<div>
+    <i class="fas fa-angle-double-right"></i>
+     <a class="cambio" id="nos" onmouseover="antes(nos)" onmouseout="despues(nos)"  href="Quienessomos.html">003</a>
+</div>
+</div>
+<div>                        
+<div>
+    <i class="fas fa-angle-double-right"></i>
+     <a class="cambio" id="contacto" onmouseover="antes(contacto)" onmouseout="despues(contacto)"  href="contacto.html">004</a>
+</div>
+</div>
+            `
+
+/*ASIDE2*/
+document.querySelector(".containeraside2").innerHTML=`
+<div class="boxaside1">
+<div><a id="icono" href="https://www.facebook.com/"><i class="fab fa-facebook-square"></i></a></div>
+<div><a id="icono" href="https://www.instagram.com/espaciob.arq/?hl=es"><i
+    class="fab fa-instagram-square"></i></a></div>
+<div><a href="mailto:spaciob.arq@gmail.com"><i class="fas fa-envelope"></i></a></div>
+</div>
+<div class="boxaside2">
+<div>
+    ES <br>PA <br> CIO <br> 
+    <div class="contlogo" >
+    <div id="lineafina"></div> <div>B.</div> 
+</div>
+</div>
+</div>
+`
+/*FOOTER*/
+document.querySelector(".containerfooter").innerHTML=`
+<div>
+<h5>   <i class="fas fa-map-marker-alt"></i> Rosario, Santa Fe, Argentina
+   | derechos reservados @ 2020
+</div>
+`
+
+
+
+/*LINKS ASIDE1*/
+
 function antes(numero) {
     let home1 = document.querySelector("#home");
     let proyectos = document.querySelector("#proyectos");
@@ -39,12 +102,5 @@ function despues(numero) {
         }
 }
 
-(function($){
-    $(window).on("load",function(){
-        $(".mCustomScrollbar").mCustomScrollbar();
-    });
-})(jQuery);
 
-$(".content").mCustomScrollbar({
-    theme:"dark"
-});
+
---
+++ hola.py
@@ -0,0 +1,8 @@
+var numero =36
+var numero =3
+if (numero/divisor > 12){
+    print("sepuede")
+
+}else{
+    print("no")
+}
